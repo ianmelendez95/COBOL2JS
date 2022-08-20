4441c49a559125294ac375b1fbb0d7645c198089
Ian Melendez <imelendez@preludedynamics.com>
2022-08-20 17:54:48 -0500

    added Megaparsec starter types

--- src/COBOL.hs
+++ src/COBOL.hs
@@ -1,12 +1,28 @@
 module COBOL where 
 
+import Data.Void
 import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
 
-newtype Prog = Prog String deriving Show
+import Text.Megaparsec
+import Text.Megaparsec.Char
+import qualified Text.Megaparsec.Char.Lexer as L
+
+type Parser = Parsec Void T.Text
+
+newtype Prog = Prog [String] deriving Show
 
 readFile :: FilePath -> IO Prog
-readFile = fmap parse . TIO.readFile
+readFile file_path = do 
+  content <- TIO.readFile file_path
+  let result = parse prog file_path content
+  case result of 
+    Left err -> error . errorBundlePretty $ err
+    Right res -> pure res
 
-parse :: T.Text -> Prog
-parse = undefined
+prog :: Parser Prog
+prog = undefined
+
+space :: Parser ()
+space = L.space space1 empty empty
+
