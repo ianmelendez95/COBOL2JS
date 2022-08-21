046e6293059b6afa841327f772eb0316fd238a0e
Ian Melendez <imelendez@preludedynamics.com>
2022-08-21 10:34:19 -0500

    initial lexers

--- src/COBOL.hs
+++ src/COBOL.hs
@@ -1,16 +1,22 @@
 module COBOL where 
 
 import Data.Void
+import Data.Char
 import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
 
 import Text.Megaparsec
-import Text.Megaparsec.Char
+import Text.Megaparsec.Char hiding (space)
 import qualified Text.Megaparsec.Char.Lexer as L
 
 type Parser = Parsec Void T.Text
 
-newtype Prog = Prog [String] deriving Show
+newtype Prog = Prog [CToken] deriving Show
+
+data CToken = Word T.Text
+            | StrLit T.Text
+            | Period
+            deriving Show
 
 readFile :: FilePath -> IO Prog
 readFile file_path = do 
@@ -23,6 +29,34 @@ readFile file_path = do
 prog :: Parser Prog
 prog = undefined
 
+ctoken :: Parser CToken
+ctoken = choice 
+  [ period
+  , strLit
+  , word
+  ]
+
+word :: Parser CToken
+word = Word <$> takeWhile1P (Just "Identifier Char") validIdentifierChar
+  where 
+    validIdentifierChar :: Char -> Bool
+    validIdentifierChar c = isDigit c || c == '-'
+
+period :: Parser CToken
+period = Period <$ char '.'
+
+strLit :: Parser CToken
+strLit = StrLit . T.pack <$> strLit'
+
+strLit' :: Parser String
+strLit' = char '"' >> manyTill L.charLiteral (char '"')
+
 space :: Parser ()
 space = L.space space1 empty empty
+
+lexeme :: Parser a -> Parser a
+lexeme = L.lexeme space
+
+symbol :: T.Text -> Parser T.Text
+symbol = L.symbol space
 
