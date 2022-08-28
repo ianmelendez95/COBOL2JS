b1d5bbda652f4749d0d62ac504c26a03975b0441
Ian Melendez <imelendez@preludedynamics.com>
2022-08-28 12:48:55 -0500

    added keyword handling

--- src/COBOL.hs
+++ src/COBOL.hs
@@ -6,6 +6,7 @@ import Control.Monad (void)
 import Data.Void
 import Data.Char
 import Data.Maybe
+import qualified Data.Set as Set
 import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
 
@@ -13,6 +14,7 @@ import Text.Megaparsec
 import Text.Megaparsec.Char hiding (space)
 import qualified Text.Megaparsec.Char.Lexer as L
 import Text.Megaparsec.Byte.Lexer (skipLineComment)
+import qualified Data.Foldable as Set
 
 type Parser = Parsec Void T.Text
 
@@ -21,6 +23,7 @@ data Prog = Prog IdentDiv DataDiv ProcDiv deriving Show
 data Statement = Display [Value]
                | Move Value T.Text
                | Compute T.Text Arith
+               | GoBack
                deriving Show
               
 data Arith = AVal Value
@@ -71,20 +74,20 @@ prog = do
 
 identification :: Parser IdentDiv
 identification = do
-  _ <- symbol "IDENTIFICATION" >> symbol "DIVISION" >> period
-  _ <- symbol "PROGRAM-ID" >> period
+  _ <- symbol kIdentification >> symbol kDivision >> period
+  _ <- symbol kProgramId >> period
   word <* period
 
 dataDivision :: Parser DataDiv
 dataDivision = do 
-  _ <- symbol "DATA" >> symbol "DIVISION" >> period
-  _ <- symbol "WORKING-STORAGE" >> symbol "SECTION" >> period
+  _ <- symbol kData >> symbol kDivision >> period
+  _ <- symbol kWorkingStorage >> symbol kSection >> period
   many (variable <* period)
 
 variable :: Parser Var
 variable =
   Var <$> (symbol "77" >> word)
-      <*> (symbol "PIC" >> variableType)
+      <*> (symbol kPic >> variableType)
       <*> between (char '(') (char ')') L.decimal
 
 variableType :: Parser VType
@@ -95,27 +98,31 @@ variableType = choice
 
 procedureDivision :: Parser ProcDiv
 procedureDivision = do
-  _ <- symbol "PROCEDURE" >> symbol "DIVISION" >> period
-  many statement <* (symbol "GOBACK" >> period)
+  _ <- symbol kProcedure >> symbol kDivision >> period
+  concat <$> many sentence
+
+sentence :: Parser [Statement]
+sentence = many statement <* period
 
 statement :: Parser Statement
 statement = choice 
   [ displayStatement
   , moveStatement
   , computeStatement
-  ] <* period
+  , GoBack <$ symbol kGoBack
+  ]
 
 displayStatement :: Parser Statement
-displayStatement = Display <$> (symbol "DISPLAY" >> some value)
+displayStatement = Display <$> (symbol kDisplay >> some value)
 
 moveStatement :: Parser Statement 
 moveStatement =
-  Move <$> (symbol "MOVE" >> value)
-       <*> (symbol "TO" >> word)
+  Move <$> (symbol kMove >> value)
+       <*> (symbol kTo >> word)
 
 computeStatement :: Parser Statement
 computeStatement = 
-  Compute <$> (symbol "COMPUTE" >> word)
+  Compute <$> (symbol kCompute >> word)
           <*> (symbol "=" >> arithmeticExpression)
 
 -- https://www.ibm.com/docs/en/cobol-zos/6.4?topic=structure-arithmetic-expressions
@@ -138,7 +145,7 @@ arithmeticExpression = choice
 
     arithVal :: Parser AVal
     arithVal = choice 
-      [ ANum 0 <$  symbol "ZERO" 
+      [ ANum 0 <$  symbol kZero 
       , ANum   <$> decimal
       , AVar   <$> word
       ]
@@ -150,11 +157,25 @@ arithmeticExpression = choice
       ]
 
 value :: Parser Value
-value = choice [StrVal <$> strLit, NumVal <$> decimal, VarVal <$> word]
+value = choice 
+  [ StrVal <$> strLit
+  , NumVal <$> decimal
+  , VarVal <$> word
+  ]
+
+-- isKeyword :: T.Text -> Bool
+-- isKeyword = 
 
 word :: Parser T.Text
-word = lexeme $ T.cons <$> first <*> rest
+word = try . lexeme $ do
+  w <- rawWord
+  if isKeyword w 
+    then failT ("Expecting word, found keyword: " <> w) 
+    else pure w
   where 
+    rawWord :: Parser T.Text
+    rawWord = T.cons <$> first <*> rest
+
     first :: Parser Char
     first = satisfy isAlpha
 
@@ -185,3 +206,44 @@ lexeme = L.lexeme space
 space :: Parser ()
 space = L.space space1 (L.skipLineComment "*") (L.skipBlockComment "/*" "*/")
 
+-- Keywords
+
+isKeyword :: T.Text -> Bool
+isKeyword t = t `Set.elem` keywords
+
+keywords :: Set.Set T.Text
+keywords = Set.fromList 
+  [ kIdentification
+  , kDivision
+  , kProgramId
+  , kWorkingStorage
+  , kData
+  , kProcedure
+  , kSection
+  , kPic
+  , kDisplay
+  , kMove
+  , kTo
+  , kCompute
+  , kZero
+  , kGoBack
+  ]
+
+kIdentification = "IDENTIFICATION"
+kDivision       = "DIVISION"
+kProgramId      = "PROGRAM-ID"
+kWorkingStorage = "WORKING-STORAGE"
+kData           = "DATA"
+kProcedure      = "PROCEDURE"
+kSection        = "SECTION"
+kPic            = "PIC"
+kDisplay        = "DISPLAY"
+kMove           = "MOVE"
+kTo             = "TO"
+kCompute        = "COMPUTE"
+kZero           = "ZERO"
+kGoBack         = "GOBACK"
+
+
+failT :: MonadFail m => T.Text -> m a
+failT = fail . T.unpack
